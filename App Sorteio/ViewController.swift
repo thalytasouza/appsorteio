01cc8b9175644c49753f0bf89285e0863c3765d3
Thalyta Souza <mayerflot@gmail.com>
2023-08-28 13:22:17 -0300

    first commit

---
+++ App Sorteio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  App Sorteio
+//
+//  Created by Thalyta on 27/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        var numero = arc4random_uniform(11)
+        legendaResultado.text = String(numero)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
